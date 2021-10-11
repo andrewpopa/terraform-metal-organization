28b133fd8569c6987718d00f245ca4efebc3da19
Andrei Popa <andrei.popa@eu.equinix.com>
2021-10-11 18:05:51 +0200

    initial code

---
+++ example/variables.tf
@@ -0,0 +1,5 @@
+variable "name"  {
+  description = "The device name"
+  type        = string
+  default     = ""
+}
---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "metal_organization" "this" {
+  name        = var.name
+  website     = var.website
+  description = var.description
+}
